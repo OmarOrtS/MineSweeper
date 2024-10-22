23da91569bbcfbb8bbe58cb4d41417d08133bac7
Test <omar.ortega102@alu.ulpgc.es>
2024-10-22 01:29:43 +0100

    Matrix class created, as well as board class only one matrix can exist for each game

---
+++ src/main/java/software/ulpgc/mineSwepper/model/Matrix.java
@@ -0,0 +1,31 @@
+package software.ulpgc.mineSwepper.model;
+
+public class Matrix {
+    private static Matrix instance;
+    protected final Cell[][] cells;
+    private final int rows;
+    private final int columns;
+
+    private Matrix(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        this.cells = new Cell[rows][columns];
+    }
+
+    public static Matrix getInstance(int rows, int columns){
+        if (instance == null) instance = new Matrix(rows,columns);
+        return instance;
+    }
+
+    public Cell[][] getCells() {
+        return cells;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+}
